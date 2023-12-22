1c08981149bd79a44b581bbcdb3cdc854266bf7f
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2023-12-22 01:12:23 -0500

    Correcting vars, rearraging code

--- calculator.js
+++ calculator.js
@@ -1,11 +1,30 @@
 const form = document.getElementById("calc-form");
 
+if (form) {
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    if (
+      isNaN(parseFloat(values.amount)) ||
+      isNaN(parseFloat(values.years)) ||
+      isNaN(parseFloat(values.rate))
+    ) {
+      alert("Invalid input! Only use numbers");
+      throw new Error("Invalid input! Only use numbers");
+    }
+    calculateLoan();
+
+    amountCalc.value = "";
+    yearsCalc.value = ""; //clearing up form for new loan
+    loanCalc.value = "";
+  });
+}
+
 function setupInitialValues() {
   const values = { amount: 10000, years: 5, rate: 2 };
   const amountCalc = document.getElementById("loan-amount");
-  amountCalc = amount.value;
+  amountCalc = values.amount;
   const yearsCalc = document.getElementById("loan-years");
-  yearsCalc = years.value;
+  yearsCalc = values.years;
   const rateCalc = document.getElementById("loan-rate");
   rateCalc = values.rate;
 }
@@ -29,31 +48,13 @@ function calculateLoan(values) {
   let rate = values.rate;
   let interest = rate / 100 / 12; // need to make interest a decimal, then make it monthly
   let totalPayments = years * 12;
-  let monthlyPayment =
+  return (
     (principle * interest) /
-    (1 - Math.pow(1 + interest, -totalPayments)).toFixed(2); //rounding to two decimal points
-
-  amountCalc.value = "";
-  yearsCalc.value = ""; //clearing up form for new loan
-  loanCalc.value = "";
+    (1 - Math.pow(1 + interest, -totalPayments)).toFixed(2)
+  ); //rounding to two decimal points
 }
 
 function updateMonthly(monthly) {
-  const monthly = document.getElementById("monthly-payment");
-  monthly.innerText = `$${monthly} per month!`;
-}
-
-if (form) {
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    if (
-      isNaN(parseFloat(loanAmount.value)) ||
-      isNaN(parseFloat(loanYears.value)) ||
-      isNaN(parseFloat(loanRate.value))
-    ) {
-      alert("Invalid input! Only use numbers");
-      throw new Error("Invalid input! Only use numbers");
-    }
-    calculateLoan();
-  });
+  const monthlyCalc = document.getElementById("monthly-payment");
+  monthlyCalc.innerText = `$${monthly} per month!`;
 }
