4952d8d6b5776cc6996a3dede5d5c505f287a07c
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2023-12-22 00:13:54 -0500

    removing console.log

--- calculator-test.js
+++ calculator-test.js
@@ -1,3 +1,5 @@
-it("should calculate the monthly rate correctly", function () {});
+it("should calculate the monthly rate correctly", function () {
+  expect(calculateLoan);
+});
 
 it("should return a result with 2 decimal places", function () {});
